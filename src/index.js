9ea86d4b3be453321e57ae11cc4525f32f935cdb
Pritam Das <49154874+pritam199902@users.noreply.github.com>
2021-04-03 20:14:12 +0530

    upload 04

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import store from "./components/store/reducer/MainReducer.js";
@@ -11,9 +11,9 @@ import store from "./components/store/reducer/MainReducer.js";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")

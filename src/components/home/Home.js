1deec9aee16e6f7c0f633f0008c0ee827efb8c88
Kousik <kousikdafarpur2015@gmail.com>
2021-04-03 21:21:06 +0530

    updated new in home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,6 +6,8 @@ function Home() {
             <h1>Home</h1>
             <h2>Testing</h2>
             <h2>Changing</h2>
+            <h3>New Change</h3>
+            <p>for pull request</p>
         </div>
     )
 }
